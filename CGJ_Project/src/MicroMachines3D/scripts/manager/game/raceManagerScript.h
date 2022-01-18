270007e57dc212f62c46f533154a8c3f206de0f6
ArcKenimuZ <arckenimuz@gmail.com>
2022-01-18 18:11:18 +0000

    Implement lives on the race manager script

--- CGJ_Project/src/MicroMachines3D/scripts/manager/game/raceManagerScript.h
+++ CGJ_Project/src/MicroMachines3D/scripts/manager/game/raceManagerScript.h
@@ -12,6 +12,19 @@
 
 class RaceManagerScript : public Script
 {
+
+private:
+	enum class GameState
+	{
+		PLAYING = 0,
+		PAUSED = 1,
+		GAMEOVER = 2,
+		RESPAWN = 3
+	};
+
+
+
+
 private:
 	static constexpr float RESET_GAME_HEIGHT = -10.0f;
 
@@ -24,17 +37,20 @@ private:
 	Entity _car = Entity();
 	RigidbodyComponent* _carRigidbody = nullptr;
 
+	CanvasComponent* _playingScreenCanvas = nullptr;
 	CanvasComponent* _pauseScreenCanvas = nullptr;
+	CanvasComponent* _gameoverScreenCanvas = nullptr;
 
 	CarMovementScript* _carMovementScript = nullptr;
 	OrangesManagerScript* _orangesManagerScript = nullptr;
 
-
-	bool _colliderWithOrange = false;
-	bool _paused = false;
 	bool _fogToggle = true;
 	bool _bumpToggle = true;
 
+	RaceManagerScript::GameState _gameState = RaceManagerScript::GameState::PLAYING;
+	unsigned int _lives = 5;
+	bool _collideLastFrame = false;
+	bool _colliderWithOrange = false;
 
 
 public:
@@ -59,46 +75,117 @@ public:
 
 	void onUpdate(float ts) override
 	{
-		// check for pause button
-		if (_eventHandler->keyState('S').pressed() || _eventHandler->keyState('s').pressed())
+		switch (_gameState)
 		{
-			Application::getInstance().setTimeScale((_paused = !_paused) ? 0.0f : 1.0f);
-			_pauseScreenCanvas->setEnabled(_paused);
+
+		case RaceManagerScript::GameState::PLAYING:
+			_checkPlayingActions();
+			break;
+
+		case RaceManagerScript::GameState::PAUSED:
+			_checkPauseActions();
+			break;
+
+		case RaceManagerScript::GameState::GAMEOVER:
+			_checkGameoverActions();
+			break;
+
+		case RaceManagerScript::GameState::RESPAWN:
+			_respawn();
+			break;
+
 		}
-
-		if (_paused)
-			return;
-
-		if (_eventHandler->keyState('R').pressed() || _eventHandler->keyState('r').pressed())
-			_respawn();
-
-		// checks for the fog toggle button
-		if (_eventHandler->keyState('F').pressed() || _eventHandler->keyState('f').pressed())
-			Renderer::setFogActive(_fogToggle = !_fogToggle);
-
-		// checks the bump toggle buttom
-		if (_eventHandler->keyState('B').pressed() || _eventHandler->keyState('b').pressed())
-			Renderer::setBumpActive(_bumpToggle = !_bumpToggle);
-
-		// check if the game needs to be reset
-		if (_colliderWithOrange || _carRigidbody->position().y < RESET_GAME_HEIGHT)
-			_respawn();
 	}
 
 
 
 
 public:
-	inline bool paused() const { return _paused; }
+	inline bool playing() const		{ return _gameState == RaceManagerScript::GameState::PLAYING; }
+	inline bool paused() const		{ return _gameState == RaceManagerScript::GameState::PAUSED; }
+	inline bool gameover() const	{ return _gameState == RaceManagerScript::GameState::GAMEOVER; }
 
 
 public:
-	inline void setColliderWithOranges(bool colliderWithOranges) { _colliderWithOrange = colliderWithOranges; }
+	void setColliderWithOranges(bool colliderWithOranges) { _colliderWithOrange = colliderWithOranges; }
 
 
 
 
 private:
+	void _checkPlayingActions()
+	{
+		if (_eventHandler->keyState('S').pressed() || _eventHandler->keyState('s').pressed())
+		{
+			_gameState = RaceManagerScript::GameState::PAUSED;
+			Application::getInstance().setTimeScale(0.0f);
+			//_playingScreenCanvas->setEnabled(false);
+			_pauseScreenCanvas->setEnabled(true);
+		}
+
+		if (_eventHandler->keyState('F').pressed() || _eventHandler->keyState('f').pressed())
+			Renderer::setFogActive(_fogToggle = !_fogToggle);
+
+		if (_eventHandler->keyState('B').pressed() || _eventHandler->keyState('b').pressed())
+			Renderer::setBumpActive(_bumpToggle = !_bumpToggle);
+
+		if (_carRigidbody->position().y < RESET_GAME_HEIGHT)
+			_gameover();
+
+		if (!_colliderWithOrange)
+			_collideLastFrame = false;
+
+		if (_colliderWithOrange && !_collideLastFrame)
+			_decreaseLives();
+
+		_colliderWithOrange = false;
+	}
+
+
+	void _checkPauseActions()
+	{
+		if (_eventHandler->keyState('S').pressed() || _eventHandler->keyState('s').pressed())
+		{
+			_gameState = RaceManagerScript::GameState::PLAYING;
+			Application::getInstance().setTimeScale(1.0f);
+			//_playingScreenCanvas->setEnabled(true);
+			_pauseScreenCanvas->setEnabled(false);
+		}
+	}
+
+	void _checkGameoverActions()
+	{
+		if (_eventHandler->keyState('R').pressed() || _eventHandler->keyState('r').pressed())
+			_prepareRespawn();
+	}
+
+
+
+
+private:
+	void _decreaseLives()
+	{
+		_collideLastFrame = true;
+		if (--_lives == 0)
+			_gameover();
+	}
+
+	void _gameover()
+	{
+		_gameState = RaceManagerScript::GameState::GAMEOVER;
+		Application::getInstance().setTimeScale(0.0f);
+		//_playingScreenCanvas->setEnabled(false);
+		_pauseScreenCanvas->setEnabled(false);
+		//_gameoverScreenCanvas->setEnabled(true);
+	}
+
+
+	void _prepareRespawn()
+	{
+		Application::getInstance().setTimeScale(1.0f);
+		_gameState = RaceManagerScript::GameState::RESPAWN;
+	}
+
 	void _respawn()
 	{
 		Transform::translateTo(_car, Coords3f({ 0.0f, CAR_BOTTOM_SIZE.y / 2.0f + CAR_FLOOR_DISTANCE + CAR_SPAWN_HEIGHT, TABLE_SIZE.z / 3.0f }));
@@ -107,7 +194,13 @@ private:
 		_carRigidbody->setAngularVelocity(Coords3f({ 0.0f, 0.0f, 0.0f }));
 		_carMovementScript->reset();
 		_orangesManagerScript->resetOrangesSpeed();
+
+		_lives = 5;
 		_colliderWithOrange = false;
+		_gameState = RaceManagerScript::GameState::PLAYING;
+		//_playingScreenCanvas->setEnabled(true);
+		_pauseScreenCanvas->setEnabled(false);
+		//_gameoverScreenCanvas->setEnabled(false);
 	}
 
 };
