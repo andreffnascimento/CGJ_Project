137a69a39d78d1cd436a68c65e8404c876ac1139
Gündersen <jmariagundersen@gmail.com>
2022-01-26 16:07:39 +0000

    Added Entity...?

--- CGJ_Project/src/MicroMachines3D/entities/world/lensFlare.h
+++ CGJ_Project/src/MicroMachines3D/entities/world/lensFlare.h
@@ -3,7 +3,24 @@
 
 #include "MicroMachines3D/common/include.h"
 
+class LensFlare : public SceneEntity 
+{
 
+public:
+	LensFlare(Scene* scene)
+		: SceneEntity(scene->createEntity("LensFlare"))
+	{
+
+		createLensFlare(scene);
+
+
+	}
+
+
+	void createLensFlare(Scene* scene) {
+		LensFlareComponent& flare = addComponent<LensFlareComponent>();
+	}
+};
 
 
 #endif // !__mm3d_entities_world_lensFlare__
